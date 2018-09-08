fef49710082b739688a77547cd09fd42b01c4877
FarFly <zhuyn1997@163.com>
2018-09-08 21:59:53 +0800

    测试DAO，数据库

--- geeklog-core/src/main/java/com/geeklog/Application.java
+++ geeklog-core/src/main/java/com/geeklog/Application.java
@@ -1,9 +1,11 @@
 package com.geeklog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.geeklog.mapper")
 public class Application {
 
     public static void main(String[] args) {
