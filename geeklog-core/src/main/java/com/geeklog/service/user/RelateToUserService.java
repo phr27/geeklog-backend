bf7298842267cf0997044a5ebde6fce60a8743f6
atKsO <atKsOption@163.com>
2018-09-19 11:28:27 +0800

    api14 17

--- geeklog-core/src/main/java/com/geeklog/service/user/RelateToUserService.java
+++ geeklog-core/src/main/java/com/geeklog/service/user/RelateToUserService.java
@@ -1,17 +1,18 @@
 package com.geeklog.service.user;
 
+import com.geeklog.domain.Comment;
 import com.geeklog.dto.ArticleDto;
 import com.geeklog.dto.Page;
 
 public interface RelateToUserService {
 
-    Page<ArticleDto> getWroteArticles(int userId);
+    Page<ArticleDto> getWroteArticles(int page, int size, int userId);
 
-    Page<ArticleDto> getStarredArticles(int userId);
+    Page<ArticleDto> getStarredArticles(int page, int size, int userId);
 
-    Page<ArticleDto> getCollectedArticles(int userId);
+    Page<ArticleDto> getCollectedArticles(int page, int size, int userId);
 
-    Page<ArticleDto> getCommentedArticles(int userId);
+    Page<Comment> getComments(int page, int size, int userId);
 
 
 }
