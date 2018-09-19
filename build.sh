85aa673b0deab01402326a851ca01e57547e6b7f
phr27 <haoran.pan@hand-china.com>
2018-09-19 14:55:23 +0800

    [ADD] nginx+ftp图片服务器

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 cd ./geeklog-core
 mvn clean package -Dmaven.test.skip=true
 cd ..
+if [ -e ~/ftpfile ];then
+    rm -rf ~/ftpfile
+fi
+mkdir -p ~/ftpfile
 docker-compose build
 docker-compose up
--- stop.sh
+++ stop.sh
@@ -6,11 +6,18 @@ fi
 
 docker cp geeklog-app-0:/log.txt $HOME/log/$(date '+%Y%m%d-%H:%M:%S')-log.txt
 
+rm -rf ~/ftpfile/
+
+docker stop geeklog-ftp-0
 docker stop geeklog-app-0
 docker stop geeklog-db-0
+docker stop geeklog-nginx-0
 
+docker rm geeklog-ftp-0
 docker rm geeklog-app-0
 docker rm geeklog-db-0
+docker rm geeklog-nginx-0
 
 docker rmi geeklog-app
 docker rmi geeklog-db
+docker rmi geeklog-nginx
